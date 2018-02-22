8718fc3fc593a87bb787a86376b7e1fb7208c832
Hari Hara Sudhan <hari.harasudhan@zohocorp.com>
2018-02-22 19:43:34 +0530

    Added ember-cli-github-pages addon

--- config/environment.js
+++ config/environment.js
@@ -53,6 +53,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/draft-51/';
     // here you can enable a production-specific feature
   }
 
